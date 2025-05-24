f5ccd2c0fbf0eeb21083a5509e2815de90661e34
cheng <crab28907@gmail.com>
2025-05-24 14:06:30 +0800

    style: unify indentation, update .gitignore

--- src/tcp_parser.c
+++ src/tcp_parser.c
@@ -3,20 +3,20 @@
 #include <stddef.h>
 
 int parser_tcp_packet(const uint8_t *packet, size_t len, tcp_header_t *h){
-	if (len < 20){
-		fprintf(stderr, "Packet too short! Got %zu , Need 20 bytes.\n", len);
-		return -1;
-	}
+    if (len < 20){
+        fprintf(stderr, "Packet too short! Got %zu , Need 20 bytes.\n", len);
+        return -1;
+    }
 
-	h->source_port = (packet[0] << 8) | packet[1];
-	h->dest_port = (packet[2] << 8 ) | packet[3];
-	h->seq_num = (packet[4] << 24) | (packet[5] << 16) 					|(packet[6] << 8) | packet[7];
-	h->ack_num = (packet[8] << 24) | (packet[9] << 16)                  |(packet[10] << 8) | packet[11];
-	h->offset_reserved_flags =  (packet[12] << 8)                                        | packet[13]; 
-	h->window_size =  (packet[14] << 8) | packet[15];
-	h->checksum =  (packet[16] << 8) | packet[17];   
-	h->urgent_pointer =  (packet[18] << 8)                                         | packet[19];  
+    h->source_port = (packet[0] << 8) | packet[1];
+    h->dest_port = (packet[2] << 8 ) | packet[3];
+    h->seq_num = (packet[4] << 24) | (packet[5] << 16)                  |(packet[6] << 8) | packet[7];
+    h->ack_num = (packet[8] << 24) | (packet[9] << 16)                  |(packet[10] << 8) | packet[11];
+    h->offset_reserved_flags =  (packet[12] << 8)                                        | packet[13]; 
+    h->window_size =  (packet[14] << 8) | packet[15];
+    h->checksum =  (packet[16] << 8) | packet[17];   
+    h->urgent_pointer =  (packet[18] << 8)                                         | packet[19];  
 
-	return 0;
+    return 0;
 }
 
