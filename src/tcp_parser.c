4ebc628842bf6986abe76d79bc48c924a5fbe0e3
cheng <crab28907@gmail.com>
2025-05-24 16:04:15 +0800

    style: remove space

--- src/tcp_parser.c
+++ src/tcp_parser.c
@@ -10,12 +10,12 @@ int parser_tcp_packet(const uint8_t *packet, size_t len, tcp_header_t *h){
 
     h->source_port = (packet[0] << 8) | packet[1];
     h->dest_port = (packet[2] << 8 ) | packet[3];
-    h->seq_num = (packet[4] << 24) | (packet[5] << 16)                  |(packet[6] << 8) | packet[7];
-    h->ack_num = (packet[8] << 24) | (packet[9] << 16)                  |(packet[10] << 8) | packet[11];
-    h->offset_reserved_flags =  (packet[12] << 8)                                        | packet[13]; 
+    h->seq_num = (packet[4] << 24) | (packet[5] << 16) | (packet[6] << 8) | packet[7];
+    h->ack_num = (packet[8] << 24) | (packet[9] << 16) | (packet[10] << 8)| packet[11];
+    h->offset_reserved_flags =  (packet[12] << 8) | packet[13]; 
     h->window_size =  (packet[14] << 8) | packet[15];
     h->checksum =  (packet[16] << 8) | packet[17];   
-    h->urgent_pointer =  (packet[18] << 8)                                         | packet[19];  
+    h->urgent_pointer =  (packet[18] << 8) | packet[19];  
 
     return 0;
 }
